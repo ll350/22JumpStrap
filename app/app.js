fd0ef5aac3454845eaca9c4c2344f0b5602a4bc5
LeoLanzo <llanzo@ncha.org>
2014-11-14 14:43:02 -0500

    Removed all dependencies except ui-bootstrap, won't work without it

--- app/app.js
+++ app/app.js
@@ -2,10 +2,6 @@
 
 // Declare app level module which depends on views, and components
 var app = angular.module('myApp', [
-  'ngRoute',
-  'myApp.view1',
-  'myApp.view2',
-  'myApp.version',
   'ui.router',
   'highcharts-ng',
   'ui.bootstrap'
